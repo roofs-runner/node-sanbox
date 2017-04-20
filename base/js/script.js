bd6e1d03b0ada02aaf6fd542d41a8b6bd7cdef09
Sergio <sergio@Sergios-MacBook-Pro.local>
2017-04-20 18:12:35 +0300

    Some new shit

---
+++ base/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(function() {
+        var gallery = $('.gallery').find('img').first();
+        var images = [
+            "images/laptop-mobile_small.jpg",
+            "images/laptop-on-table_small.jpg",
+            "images/people-office-group-team_small.jpg"
+        ];
+
+        var i = 0;
+        setInterval(function () {
+            i = (i + 1) % images.length;
+            gallery.fadeOut(function () {
+                $(this).attr('src', images[i]);
+                $(this).fadeIn();
+            });
+            console.log(gallery.attr('src'));
+        }, 2000);
+    });
+});
